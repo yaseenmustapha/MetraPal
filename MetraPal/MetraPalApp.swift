a56570db13a823b90722a3fd4912a47276a63d2f
Yaseen <yaseenmust@gmail.com>
2024-05-05 19:58:44 -0500

    Initial Commit

---
+++ MetraPal/MetraPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetraPalApp.swift
+//  MetraPal
+//
+//  Created by Yaseen Mustapha on 5/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MetraPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
